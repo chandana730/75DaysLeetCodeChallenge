237c4e3f93d2c6d2f5adf742c2358c820ccda383
chandana730 <pgowrichandana@gmail.com>
2026-05-22 19:32:52 +0530

    Time: 24 ms (31.33%), Space: 52.1 MB (73.52%) - LeetHub

---
+++ 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.java
@@ -0,0 +1,19 @@
+import java.util.PriorityQueue;
+class KthLargest {
+    private final int k;
+    private final PriorityQueue<Integer> minHeap;
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        minHeap = new PriorityQueue<>();
+        for (int num : nums) {
+            add(num);
+        }
+    }
+    public int add(int val) {
+        minHeap.offer(val);
+        if (minHeap.size() > k) {
+            minHeap.poll();
+        }
+        return minHeap.peek();
+    }
+}
